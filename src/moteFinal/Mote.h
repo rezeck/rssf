8db5999d6aad14e9360cd773f5f8c3c1fbc71e06
ubuntu <ubuntu@ubuntu.(none)>
2016-06-09 04:04:40 -0300

    Final version of mote and BS

---
+++ src/moteFinal/Mote.h
@@ -0,0 +1,38 @@
+#ifndef MOTE_H
+#define MOTE_H
+//#define TOSSIM_BASESTATION_SIMULATION
+//#define RSSF_DEBUG
+enum {
+	// Message type
+	AM_QUESTION = 0xF0,
+	AM_ANSWER = 0x0F,
+	
+	// Msg type to use in TOSSIM
+	AM_MOTEMSG = AM_QUESTION
+};
+
+typedef nx_struct MoteMsg {
+	// Header
+	nx_uint16_t version;
+	nx_uint16_t size;
+	
+	// Required payload data
+	nx_uint16_t temperature;
+	nx_uint16_t luminosity;
+	
+	// Optional payload data
+	nx_uint16_t src; // half of extra_data_1
+	nx_uint16_t parent_node; // half of extra_data_1
+	nx_uint32_t extra_data_2;
+	nx_uint32_t extra_data_3;
+	nx_uint32_t extra_data_4;
+	nx_uint32_t extra_data_5;		
+	
+} MoteMsg;
+
+typedef nx_struct SensorData {
+	nx_uint16_t photo;
+	nx_uint16_t temp;
+} SensorData;
+
+#endif /* MOTE_H */
---
+++ src/moteFinal/simulator.py
@@ -0,0 +1,62 @@
+#! /usr/bin/python
+import sys, time
+from TOSSIM import *
+from MoteMsg import *
+
+numberOfNodes = 3
+tcpPort = 9002
+
+t = Tossim([])
+m = t.mac()
+r = t.radio()
+sf = SerialForwarder(tcpPort)
+throttle = Throttle(t, 10)
+
+t.addChannel("Boot", sys.stdout)
+t.addChannel("Radio", sys.stdout)
+t.addChannel("Serial", sys.stdout)
+
+print "Booting nodes"
+for i in range(numberOfNodes):
+  m = t.getNode(i)
+  m.bootAtTime((31 + t.ticksPerSecond() / 10) * i + 1)
+
+print "Starting Serial"
+sf.process()
+throttle.initialize()
+
+print "Defining topology"
+f = open("topo.txt", "r")
+for line in f:
+  s = line.split()
+  if s:
+    r.add(int(s[0]), int(s[1]), float(s[2]))
+
+print "Defining noise"
+noise = open("meyer-heavy.txt", "r")
+for line in noise:
+  s = line.strip()
+  if s:
+    val = int(s)
+    for i in range(numberOfNodes):
+      t.getNode(i).addNoiseTraceReading(val)
+
+for i in range(numberOfNodes):
+  t.getNode(i).createNoiseModel()
+
+# run events forever
+while True:
+  throttle.checkThrottle()
+  t.runNextEvent()
+  sf.process()
+  time.sleep(0.1)
+
+# deliver a packet
+#msg = MoteMsg()
+#msg.set_version(2)
+#msg.set_size(2)
+#pkt = t.newPacket()
+#pkt.setData(msg.data)
+#pkt.setType(240)
+#pkt.setDestination(65535)
+#pkt.deliver(0, t.time() + 10)
